7670b408d34375796f60c521bbb656a8c6d91f50
彭中华 <>
2019-03-22 10:26:10 +0800

    导航和侧边栏设置

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# vuepress4gitpage
+# 这是一个用 vuepress 构建的项目
+
+# 默认部署到 pengzhonghua.github.io 项目下的 master 分支下
+# 配置部署的文件是 deploy.sh
+
+# 初衷是用来写技术文章，随笔
+
+# 使用方法：
+1、 本地运行：yarn docs:dev
+    打包编译：yarn docs:build 
+
+# 部署方法：
+1、 进入到项目根文件下
+2、 执行命令 yarn deploy
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,22 @@
 module.exports = {
-  title: 'Hello VuePress ~',
-  description: 'Just playing around'
+  title: '个人主页',
+  description: '彭中华的空间',
+  themeConfig: {
+    nav: [
+      { text: '主页', link: '/'},
+      { text: '博文', items: [
+        { text: 'Vue', link: '/vue/'},
+        { text: 'Angular', link: '/angular/'},
+        { text: 'React', link: '/react/'}
+      ]},
+      { text: '关于', link: '/about/'}
+    ],
+    sidebar: {
+      '/vue/': [ "", "vue-router", "vuex"],
+      '/angular/': [ "", "angularJs", "angular"],
+      '/react/': [ "", "redux", "react"]
+    },
+    sidebarDepth: 2,
+    lastUpdated: 'Last Updated'
+  }
 }
